f956b3069bbf70626d8850f12e845724685284b7
Leonardo <leonardo.ribeiro@gee.inatel.br>
2020-12-28 08:06:00 -0300

    Update to show occupancy

--- line_read.c
+++ line_read.c
@@ -15,9 +15,12 @@ int main(int argc, char *argv[])
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
-    float mean;
-    uint32_t counter;
+    float mean = 0;
+    uint32_t counter = 0;
     char str[80];
+    /*occupancy*/
+    uint32_t occ_counter = 0;
+    uint8_t occupancy = 0;
 
     meas_file = fopen("../mestrado_file_plot/meas.txt", "w+");
     //meas_file = fopen("meas.txt", "w+");
@@ -26,6 +29,7 @@ int main(int argc, char *argv[])
     {
         counter = 0;
         mean = 0;
+        occ_counter = 0;
 
         printf("argv %d: %s\n", i, argv[i]);
 
@@ -40,19 +44,26 @@ int main(int argc, char *argv[])
             //printf("%s", line);
             if (isdigit(line[1]))
             {
-                float i = atof(line);
+                float j = atof(line);
                 //printf("%02f\n", i);
-                counter++;
-                mean += i;
+                if(j > (-150)) {
+                    counter++;
+                    mean += j;
+                }
+                if(j > (-65)) {
+                    occ_counter++;
+                }
                 //printf("mean %02f\n", mean);
             }
         }
         mean /= counter;
+        occupancy = (occ_counter/counter) * 100;
+        //printf("occupancy: %02d \n", occupancy);
         //printf("final mean %02f\n", mean);
-        sprintf(str, "%02d,%f\n", (i - 1), mean);
+        sprintf(str, "%02d,%f,%03d\n", (i - 1), mean, occupancy);
         printf("%s", str);
         if(i < 100) {
-            fwrite(str , 1 , 14 , meas_file );
+            fwrite(str , 1 , 18 , meas_file );
         }
             
         memset(str, 0x00, 80);
